9967cde9764564c3c1e8b14872a4fd6ea34b65d8
unknown <thevaki.paramu@gmail.com>
2020-01-12 18:45:41 +0530

    Adding code to reand and write files

---
+++ FileOperation/String/WriteFileAsString.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteFileAsString {
+	private static void writeUsingOutputStream(String data) {
+        OutputStream outputStream = null;
+        try {
+        	outputStream = new FileOutputStream(new File("FILE PATH"));
+        	outputStream.write(data.getBytes(), 0, data.length());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+            	outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+	
+	public static void main(String[] args) {
+        String data = "Hi..Welcome to java file writing";
+
+        writeUsingOutputStream(data);
+        System.out.println("Finished Writing data to File");
+    }
+}
