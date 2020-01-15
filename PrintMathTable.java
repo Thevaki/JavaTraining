9d5f2b8ee7563580ea4360cceaf49ac00524afaa
unknown <thevaki.paramu@gmail.com>
2020-01-15 17:36:51 +0530

    Alignment changes in print math table

--- PrintMathTable.java
+++ PrintMathTable.java
@@ -1,9 +1,18 @@
 
 public class PrintMathTable{
 	public static void main(String[] args) {
+		
+		System.out.printf("  ");
 		for (int i = 1; i <= 9; i++) {
+		System.out.printf("%5s",i);
+		}
+		
+		System.out.println();
+		System.out.println("-----------------------------------------------");
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%2s",i + " |");
 			for (int j = 1; j <= 9; j++) {
-				System.out.print(i * j + " ");
+				System.out.printf("%5s",i * j + " ");
 			}
 			System.out.println();
 		}
