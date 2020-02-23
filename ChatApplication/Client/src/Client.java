3c77ff7d31f9270e1e7ebe67ac168c904b26ba73
unknown <thevaki.paramu@gmail.com>
2020-02-23 21:22:55 +0530

    Chat Application code

---
+++ ChatApplication/Client/src/Client.java
@@ -0,0 +1,53 @@
+
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    private String hostname;
+    private int port;
+    private String userName;
+
+    public Client(String hostname, int port,String userName) {
+        this.hostname = hostname;
+        this.port = port;
+        this.userName=userName;
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 3)
+            return;
+
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+        String userName = args[2];
+
+        Client client = new Client(hostname, port,userName);
+        client.execute();
+    }
+
+    public void execute() {
+        try {
+            Socket socket = new Socket(hostname, port);
+
+            System.out.println("Connected to the chat server");
+
+            new ReadConnection(socket, this).start();
+            new SendConnection(socket, this).start();
+
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+
+    }
+
+    void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    String getUserName() {
+        return this.userName;
+    }
+
+}
