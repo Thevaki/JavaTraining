2938bc378a468f110e76b3de3b691f9cf3e539d2
User <sathurshannn@gmail.com>
2020-03-12 01:19:22 +0530

    jwt token implementation

--- LMS_V2/User/src/main/java/com/libraryManagementSystem/User/Controller/UserController.java
+++ LMS_V2/User/src/main/java/com/libraryManagementSystem/User/Controller/UserController.java
@@ -1,8 +1,17 @@
 package com.libraryManagementSystem.User.Controller;
 
 import com.libraryManagementSystem.User.Model.User;
+import com.libraryManagementSystem.User.SecurityConfig.AuthenticationResponse;
+import com.libraryManagementSystem.User.SecurityConfig.JwtUtil;
+import com.libraryManagementSystem.User.Service.UserLoginService;
 import com.libraryManagementSystem.User.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.bind.annotation.XmlRootElement;
@@ -15,6 +24,33 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserLoginService userLoginService;
+
+    @RequestMapping(value="/authenticate",method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody User user) throws Exception {
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        }catch (BadCredentialsException e){
+            throw new Exception("Incorrect username or password",e);
+        }
+
+        final UserDetails userDetails =
+                userLoginService.loadUserByUsername(user.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt) {
+        });
+    }
 
     @RequestMapping(value = "/createUser" , method = RequestMethod.POST)
     public User createUser(@RequestBody User user){
--- LMS_V2/User/src/main/java/com/libraryManagementSystem/User/SecurityConfig/SecurityConfig.java
+++ LMS_V2/User/src/main/java/com/libraryManagementSystem/User/SecurityConfig/SecurityConfig.java
@@ -4,13 +4,16 @@ import com.libraryManagementSystem.User.Service.UserLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -27,6 +30,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                anyRequest().authenticated();
+
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     //@Override
     //protected void configure(HttpSecurity http) throws Exception {
         //http
