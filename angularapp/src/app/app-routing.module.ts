8bbe4e34b39e784ca838325ee47f8ec91fe11043
Thevaki <thevaki.paramu@gmail.com>
2020-03-14 03:28:27 +0530

    created login page and Book page

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+//import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+// const routes: Routes = [
+//   {
+//     path: '',    
+//     component: AppComponent,
+//     children: [
+//       { path: 'books', loadChildren: './books/books.component' }
+//       //{ path: 'drivers', loadChildren: './drivers/drivers.module#DriversModule' },
+// //  },
+//     ]}
+// ];
+
+const routes: Routes = [
+  {
+    path: 'books',    
+    component: BooksComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+//import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+
+// const routes: Routes = [  
+//   {  
+//     path: 'books',  
+//     component: BooksComponent  
+//   }
+// ];  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  //exports: [RouterModule]  
 })
 export class AppModule { }
+//export class AppRoutingModule { } 
